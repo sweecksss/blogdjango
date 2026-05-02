2770999dd508f0d16ab1579079cfbce0c9460284
sweecksss <todoresevv@gmail.com>
2026-05-02 19:19:23 +0300

    Added comments

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Post, Author
+from .models import Post, Author, Comment
 
 # Register your models here.
-admin.site.register(Post)
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInLine]
+    
 admin.site.register(Author)
+admin.site.register(Comment)
--- app/models.py
+++ app/models.py
@@ -24,3 +24,12 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author_name} on {self.post.title}'
